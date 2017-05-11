994b0db88e13ecd127f18e467825dca7beb976f8
Deepak Kumar <kumarde@umich.edu>
2017-05-11 16:30:23 -0500

    responder actually responds

--- responder.py
+++ responder.py
@@ -1,19 +1,22 @@
 from scapy.all import *
 import sys
 
-a = sniff(count=1,filter="tcp and tcp[tcpflags] == tcp-syn and src host 10.192.190.193")
-packet = a[TCP][0]
-src_ip = packet[IP][0].src
-dst_ip = packet[IP][0].dst
+def main():
+    host = sys.argv[1]
+    while True:
+        a = sniff(count=1,filter="tcp and tcp[tcpflags] == tcp-syn and dst host " + sys.argv[1])
+        packet = a[TCP][0]
+        src_ip = packet[IP][0].src
+        dst_ip = packet[IP][0].dst
 
-print(src_ip)
-print(dst_ip)
+        dport = packet.sport
+        SeqNr = packet.seq
+        AckNr = packet.seq + 1
 
-dport = packet.sport
-SeqNr = packet.seq
-AckNr = packet.seq + 1
+        ip=IP(src=dst_ip, dst=src_ip)
+        TCP_SYNACK=TCP(sport=a[0].dport, dport=dport, flags="SA", seq=SeqNr, ack=AckNr, options=[('MSS', 1460)])
 
-ip=IP(src=dst_ip, dst=src_ip)
-TCP_SYNACK=TCP(sport=a[0].dport, dport=dport, flags="SA", seq=SeqNr, ack=AckNr, options=[('MSS', 1460)])
+        ack = sendp(ip/TCP_SYNACK)
 
-ANSWER=sr1(ip/TCP_SYNACK)
+if __name__ == "__main__":
+    main()
